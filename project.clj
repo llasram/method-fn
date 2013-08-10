c814a5db10f96c6ba1f8fb299eb223e18a71469b
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2013-08-10 10:18:53 -0400

    Release version 0.1.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.platypope/method-fn "0.1.0-SNAPSHOT"
+(defproject org.platypope/method-fn "0.1.0"
   :description "A Clojure library augmenting Java methods as functions"
   :url "http://github.com/llasram/method-fn"
   :license {:name "Eclipse Public License"
