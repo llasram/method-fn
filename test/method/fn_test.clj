020213b7f8c0854449e841c567b189efa6835d36
Marshall Bockrath-Vandegrift <llasram@gmail.com>
2014-02-12 19:09:15 -0500

    Add tests for new features.

--- test/method/fn_test.clj
+++ test/method/fn_test.clj
@@ -1,6 +1,6 @@
 (ns method.fn-test
   (:require [clojure.test :refer :all]
-            [method.fn]))
+            [method.fn :as mf]))
 
 (def expected
   ["foo" "bar" "baz"])
@@ -10,12 +10,21 @@
 
 (deftest test-instance
   (is (= expected (map #mf/i trim words)))
-  (is (= expected (map #mf/i String/trim words))))
+  (is (= expected (map (mf/i trim) words)))
+  (is (= expected (map #mf/i String/trim words)))
+  (is (= expected (map (mf/i String/trim) words))))
 
 (deftest test-static
   (is (= expected (map #mf/s String/valueOf expected)))
+  (is (= expected (map (mf/s String/valueOf) expected)))
   (is (= [1 2 4 8 16] (map (comp long #mf/s Math/pow)
                            (repeat 2) (range 5)))))
+
+(deftest test-constructor
+  (is (= (map biginteger (range 5))
+         (map (comp #mf/c BigInteger str) (range 5))))
+  (is (= (map biginteger (range 5))
+         (map (comp (mf/c BigInteger) str) (range 5)))))
 
 (deftest test-caching
   (is (identical? (class #mf/i String/trim)
